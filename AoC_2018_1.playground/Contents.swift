3ca81ca915f7e3f368a8142b2faf106fa0ad234a
Mike Dockerty <mdockert@nerdery.com>
2018-12-01 09:47:02 -0600

    Solution to Day 1

--- AoC_2018_1.playground/Contents.swift
+++ AoC_2018_1.playground/Contents.swift
@@ -1,1 +1,35 @@
-print(input(filename: "sample_data"))
+// Read and format puzzle input
+var stringInput = input(filename: "puzzle_input_1_1")
+stringInput.removeAll(where: {$0 == "+"})
+let splitString = stringInput.split(separator: "\n")
+let puzzleInput = splitString.map { Int($0)! }
+
+
+// Puzzle #1
+
+var frequency = 0
+
+for changeAmount in puzzleInput {
+    frequency += changeAmount
+}
+
+print(frequency)
+
+
+// Puzzle #2
+
+frequency = 0
+var frequencySet = Set([frequency])
+var duplicateFrequencyFound = false
+
+while !duplicateFrequencyFound {
+    for changeAmount in puzzleInput {
+        frequency += changeAmount
+        if frequencySet.contains(frequency) {
+            print("Duplicate frequency: \(frequency)")
+            duplicateFrequencyFound = true
+        } else {
+            frequencySet.insert(frequency)
+        }
+    }
+}
--- AoC_2018_1.playground/Sources/AoC_2018_shared.swift
+++ AoC_2018_1.playground/Sources/AoC_2018_shared.swift
@@ -2,7 +2,7 @@ import Foundation
 
 public func input(filename: String) -> String {
     do {
-        guard let fileUrl = Bundle.main.url(forResource: filename, withExtension: "") else {
+        guard let fileUrl = Bundle.main.url(forResource: filename, withExtension: "txt") else {
             print("Couldn't open '\(filename)'")
             fatalError() }
         let text = try String(contentsOf: fileUrl, encoding: String.Encoding.utf8)
