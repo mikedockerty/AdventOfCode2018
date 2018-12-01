2500d6805f134a7284c3ed540f11bce399718349
Mike Dockerty <mdockert@nerdery.com>
2018-12-01 10:17:28 -0600

    Added playground for Day 2

---
+++ AoC_2018_2.playground/Sources/AoC_2018_shared.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+public func input(filename: String) -> String {
+    do {
+        guard let fileUrl = Bundle.main.url(forResource: filename, withExtension: "") else {
+            print("Couldn't open '\(filename)'")
+            fatalError() }
+        let text = try String(contentsOf: fileUrl, encoding: String.Encoding.utf8)
+        return text
+    } catch {
+        print(error)
+        return ""
+    }
+}
